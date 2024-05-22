e81e9384cc51aba5e537f2cf857fd134778b1957
Aman <amangarg0303@gmail.com>
2024-05-22 15:51:58 +0530

    home and dashboard layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,57 @@
 import "./App.css";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Dashboard from "./pages/dashboard/Dashboard";
+import { Sidebar } from "./components/sidebar/Sidebar";
+import Analytics from "./pages/analytics/Analytics";
 
 function App() {
+  const HomeLayout = () => {
+    return (
+      <div className="App">
+        <Outlet />
+      </div>
+    );
+  };
+
+  const DashboardLayout = () => {
+    return (
+      <div>
+        <Sidebar />
+        <Outlet />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
+    },
+    {
+      path: "/dashboard",
+      element: <DashboardLayout />,
+      children: [
+        {
+          path: "/dashboard/",
+          element: <Dashboard />,
+        },
+        {
+          path: "/dashboard/analytics",
+          element: <Analytics />,
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <Home />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
